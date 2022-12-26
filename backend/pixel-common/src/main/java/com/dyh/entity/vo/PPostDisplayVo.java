428db422661c0436ae3711d68c11ed7394a53a91
pixel-revolve <pixel-revolve@qq.com>
2022-12-26 22:04:09 +0800

    后端项目重命名

---
+++ backend/pixel-common/src/main/java/com/dyh/entity/vo/PPostDisplayVo.java
@@ -0,0 +1,37 @@
+package com.dyh.entity.vo;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class PPostDisplayVo {
+    //主题ID
+    private Long id;
+    //用户ID
+    private Long userId;
+    //用户昵称
+    private String nickname;
+    //评论数
+    private Long commentCount;
+    //收藏数
+    private Long collectionCount;
+    //点赞数
+    private Long upvoteCount;
+    //查看数
+    private Long watchCount;
+    //摘要
+    private String summary;
+    //最新回复时间
+    private Long latestRepliedOn;
+    //标签
+    private String tags;
+    //附件价格(分)
+    private Long attachmentPrice;
+    //IP地址
+    private String ip;
+    //IP城市地址
+    private String ipLoc;
+    //创建时间
+    private Date createdOn;
+}
--- web/pixel-common/src/main/java/com/dyh/constant/UserConstants.java
+++
@@ -1,6 +0,0 @@
-package com.dyh.constant;
-
-public class UserConstants {
-    public static final String USERNAME_PREFIX = "user";
-    public static final String USER_NICKNAME_PREFIX = "user_";
-}
