ecd2fac6feffd72435c10790b3613d40d89c0936
pixel-revolve <pixel-revolve@qq.com>
2023-03-16 10:22:45 +0800

    更新测试模块

---
+++ web/pixel-test/publisher/src/test/java/com/dyh/SpringAmqpTest.java
@@ -0,0 +1,26 @@
+package com.dyh;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringAmqpTest {
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    @Test
+    public void testSimpleQueue() {
+        // 队列名称
+        String queueName = "simple.queue";
+        // 消息
+        String message = "hello, spring amqp!";
+        // 发送消息
+        rabbitTemplate.convertAndSend(queueName, message);
+    }
+}
